83b041eaf836af44038d116942b67bb5f5d9bb1c
Jari Jääskelä <jari.jaaskela@hotmail.com>
2013-06-14 01:30:59 +0300

    Update

---
+++ src/scripts/metafisher/enums/States.java
@@ -0,0 +1,20 @@
+package scripts.metafisher.enums;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jari
+ * Date: 6.6.2013
+ * Time: 20:07
+ * To change this template use File | Settings | File Templates.
+ */
+public enum States {
+    IDLE,
+    GUI,
+    FISH,
+    DROP,
+    BANK,
+    WALK_TO_BANK,
+    WALK_TO_FISH,
+    INV_CHANGE,
+    WITHDRAW_TOOLS;
+}
---
+++ src/scripts/metafisher/methods/Fish.java
@@ -0,0 +1,61 @@
+package scripts.metafisher.methods;
+
+import org.tribot.api.DynamicClicking;
+import org.tribot.api2007.NPCs;
+import org.tribot.api2007.Player;
+import org.tribot.api2007.Walking;
+import org.tribot.api2007.types.RSNPC;
+import scripts.metafisher.enums.FishPools;
+import scripts.metafisher.enums.FishTools;
+import util.Timing;
+
+import static org.tribot.api.General.random;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jari
+ * Date: 7.6.2013
+ * Time: 18:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Fish {
+
+    private FishPools POOL;
+    private FishTools TOOL;
+
+    public Fish(FishPools POOL, FishTools TOOL) {
+        this.POOL = POOL;
+        this.TOOL = TOOL;
+    }
+
+    public boolean startFishing() {
+        final RSNPC pool[] = NPCs.findNearest(POOL.getID());
+        if (pool[0].isOnScreen()) {
+            if (!pool[0].isInteractingWithMe()) {
+
+
+                if (DynamicClicking.clickRSNPC(pool[0], TOOL.getOption())) {
+                    Timing.CSleep(new Timing.Condition() {
+                        @Override
+                        public boolean validate() {
+                            return pool[0].isInteractingWithMe();
+                        }
+                    }, random(4000, 5000));
+                    return true;
+                }
+
+
+            }
+        } else {
+            Walking.walkTo(pool[0].getPosition());
+            Timing.CSleep(new Timing.Condition() {
+                @Override
+                public boolean validate() {
+                    return !Player.isMoving();
+                }
+            }, 5000);
+        }
+
+        return false;
+    }
+}
